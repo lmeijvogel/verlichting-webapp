689068d9fdc38bbc90c1667880d4db27e634a6ce
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2015-12-06 17:58:35 +0100

    Create Gruntfile that copies everything and minifies JS.
    This is not yet a solution for development, since it does not do live reloading.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,20 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    copy: {
+      main: {
+        files: [
+          {cwd: 'source/', src: ['javascripts/bootstrap.min.js'], dest: 'site/', expand: true },
+          {cwd: 'source/', src: ['index.html'], dest: 'site/', expand: true },
+          {cwd: 'source/', src: ['stylesheets/*.css'], dest: 'site/', nonull: true, expand: true },
+          {cwd: 'source/', src: ['images/*'], dest: 'site/', nonull: true, expand: true },
+        ]
+      }
+    },
+    eslint: {
+      src: ["source/javascripts/my_zwave/*.js"]
+    },
     browserify: {
       'tmp/my_zwave.js': ['source/javascripts/my_zwave/start.js']
     },
@@ -8,15 +22,19 @@ module.exports = function (grunt) {
       options: {
         banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      build: {
-        src: 'tmp/my_zwave.js',
-        dest: 'build/my_zwave.min.js'
+      my_target: {
+        files: {  'site/javascripts/my_zwave.min.js': ['tmp/my_zwave.js'] }
       }
-    }
+    },
+
+    clean: ['site/']
   });
 
+  grunt.loadNpmTasks('gruntify-eslint');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', ['browserify', 'uglify']);
+  grunt.registerTask('default', ['eslint', 'browserify', 'uglify', 'copy']);
 };
