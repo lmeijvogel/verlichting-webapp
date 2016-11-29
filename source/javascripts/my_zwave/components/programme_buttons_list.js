be9dbd1de2f7e3077c7b16e15185a727f68e5f19
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2016-11-29 21:03:40 +0100

    Inline the programme_buttons_list template in the corresponding JS file.
    It was the only component with the template inlined in index.html.

--- source/javascripts/my_zwave/components/programme_buttons_list.js
+++ source/javascripts/my_zwave/components/programme_buttons_list.js
@@ -1,7 +1,6 @@
 var Vue = window.Vue;
 
 module.exports = Vue.component('programme-buttons-list', {
-  template: '#programme-buttons-list',
   props: ['programmes', 'activeProgrammeId'],
   computed: {
     loaded: function () { return this.programmes !== undefined; }
@@ -10,5 +9,23 @@ module.exports = Vue.component('programme-buttons-list', {
     programmeRequested: function (programme) {
       this.$emit('programme-requested', programme);
     }
-  }
+  },
+  template: '' +
+    '<div class="mdl-cell--12-col mdl-cell--4-col-desktop mdl-card mdl-shadow--2dp programme-buttons-list">' +
+      '<div class="mdl-card__title">' +
+        '<h3 class="mdl-card__title-text">Programma\'s</h3>' +
+      '</div>' +
+      '<div class="mdl-card__supporting-text">' +
+        '<div v-if="!loaded" class="loading-spinner-container">' +
+          '<div class="loading-spinner mdl-spinner mdl-js-spinner is-active"></div>' +
+        '</div>' +
+        '<div v-for="programme of programmes">' +
+          '<programme-button ' +
+            'v-bind:programme="programme" ' +
+            'v-bind:active-programme-id="activeProgrammeId" ' +
+            'v-on:selected="programmeRequested(programme)">' +
+          '</programme-button>' +
+        '</div>' +
+      '</div>' +
+    '</div>'
 });
